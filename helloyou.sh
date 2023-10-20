0db08fb10a89ba62ba7ebf72995d9fb483718624
enrique salceda <enriquesalceda@gmail.com>
2023-10-21 01:09:42 +1100

    add clean, find mobster and refactor the case statement
    Co-authored-by: Enrique Salceda <enriquesalceda@proton.me>
    Co-authored-by: Chilli Sauce <chilli.sauce.and.pickles@gmail.com>
    Card: [PD-111]

--- helloyou.sh
+++ helloyou.sh
@@ -2,7 +2,12 @@
 
 readonly CO_AUTHOR_TRAILER_TOKEN="Co-authored-by"
 readonly CARD_TRAILER_TOKEN="Card"
-readonly USAGE="usage: git idc"
+readonly USAGE="usage: git caravan show all trailer in commit template
+  or: git caravan -m <initials>...      add the mob that contributed to the commit
+  or: git caravan -c <card>             add the jira, trello, whatever you use card
+  or: git caravan -w                    wash the caravan! clean all trailers
+  or: git caravan -fm                   find mobster
+"
 
 # Initialize an empty array for users and an empty string for card
 users=()
@@ -16,14 +21,22 @@ main() {
     exit 0
   fi
 
+  if [[ $# -eq 1 ]]; then
+    if [[ $1 == "-w" || $1 == "--wash"  ]]; then
+      wash_the_caravan
+    fi
+
+    if [[ $1 == "-fm" || $1 == "--findmobsters"  ]]; then
+      find_mobsters
+    fi
+  fi
+ 
+
   while [[ $# -gt 0 ]]; do
       key="$1"
       
       case $key in
-          clean)
-            remove_trailers
-            ;;
-          -a)
+          -m|--mob)
               shift # remove -a from the list of arguments
               # While there are arguments and the next one doesn't start with '-'
               while [[ $# -gt 0 ]] && [[ "$1" != -* ]]; do
@@ -31,7 +44,7 @@ main() {
                   shift # remove the current user from the list of arguments
               done
               ;;
-          -c)
+          -c|--card)
               card="$2"
               shift # remove -c
               shift # remove its argument
@@ -47,7 +60,7 @@ main() {
 }
 
 current_trailers() {
-  ensure_template_file
+  ensure_git_commit_template_exists
   print_trailers
   exit 0
 }
@@ -57,7 +70,7 @@ print_trailers() {
   sed -n "/$CARD_TRAILER_TOKEN/p" "$template_file"
 }
 
-ensure_template_file() {
+ensure_git_commit_template_exists() {
   must_have_config "commit.template" "commit template is not configured
 
 Example:
@@ -92,12 +105,13 @@ add_co_authors() {
 }
 
 add_card() {
-  git interpret-trailers --trailer "$CARD_TRAILER_TOKEN: $card" --in-place "$template_file"
+  uppercase_card=$(echo "$card" | tr 'a-z' 'A-Z')
+  git interpret-trailers --trailer "$CARD_TRAILER_TOKEN: [$uppercase_card]" --in-place "$template_file"
 }
 
 update_trailers() {
-  ensure_template_file
-  remove_trailers
+  ensure_git_commit_template_exists
+  find_and_remove_current_trailers
 
   if [[ $users ]]; then
     add_co_authors
@@ -112,17 +126,25 @@ update_trailers() {
   exit 0
 }
 
-remove_trailers() {
-  ensure_template_file
-  remove_trailers
+wash_the_caravan() {
+  ensure_git_commit_template_exists
+  find_and_remove_current_trailers
   exit 0
 }
 
-remove_trailers() {
+find_and_remove_current_trailers() {
   local temp_file
   temp_file=$(mktemp)
   sed -e "/$CARD_TRAILER_TOKEN/d" -e "/$CO_AUTHOR_TRAILER_TOKEN/d" "$template_file" > "$temp_file"
   mv "$temp_file" "$template_file"
+}
+
+find_mobsters() {
+  {
+    git log --pretty="%an <%ae>";
+    git log --pretty="%(trailers:key=Co-authored-by,valueonly,only)" | awk NF;
+  } | sort | uniq
+  exit 0
 }
 
 main "$@"
